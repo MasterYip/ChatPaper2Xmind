c5ed1326c7c6492181347301d820464914da4bcb
MasterYip <2205929492@qq.com>
2023-08-09 14:15:03 +0800

    feat: improve compatibility for various journals & add PDFFigure2 for image&table generation

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
 """OpenAI API"""
-from re import I
-from private_config import PRIVATE_KEY, PRIVATE_APIBASE
-# APIBASE = "https://api.openai.com/v1/engines/"  # TODO:OpenAI API base url
-APIBASE = PRIVATE_APIBASE
-APIKEYS = PRIVATE_KEY         # Your OpenAI API keys
+APIBASE = "https://api.openai.com/v1/engines/"
+APIKEYS = [""]                  # Your OpenAI API keys
 MODEL = "gpt-3.5-turbo"         # GPT model name
 LANGUAGE = "English"            # Only partially support Chinese
 KEYWORD = "Science&Engineering" # Keyword for GPT model (What field you want the model to focus on)
@@ -18,14 +15,14 @@ GEN_IMGS = True
 GEN_EQUATIONS = True
 
 # PDFFigure2
-USE_PDFFIGURE2 = True     # IMPORTANT: This requires you to install Java
-SNAP_WITH_CAPTION = True  # Only valid when USE_PDFFIGURE2 is True
+USE_PDFFIGURE2 = True       # Use PDFFigure2 to generate images & tables (This requires you to install JVM)
+SNAP_WITH_CAPTION = True    # Generate images & tables with caption (Only valid when USE_PDFFIGURE2 is True)
 
 # Max generation item number
-TEXT2LIST_MAX_NUM = 4
-TEXT2TREE_MAX_NUM = 4
-FAKE_GPT_RESPONSE = ""
-if False:  # Use true GPT model
+TEXT2LIST_MAX_NUM = 4       # Max number of items for each list
+TEXT2TREE_MAX_NUM = 4       # Max number of subtopics for each topic
+FAKE_GPT_RESPONSE = "Fake"  # Fake GPT response when GPT_ENABLE is False
+if True:  # Use true GPT model
     GPT_ENABLE = True
     THREAD_RATE_LIMIT = 100       # Each APIKEY can send 3 requests per minute (limited by OpenAI)
 else:    # Use fake GPT model
@@ -33,14 +30,13 @@ else:    # Use fake GPT model
     THREAD_RATE_LIMIT = 6000  
 
 
-"""PDF Parser"""
-# Regex match string for section title
-# Special
+"""PDF Parser - Regular Expression"""
+# Special title
 ABS_MATCHSTR = "ABSTRACT|Abstract|abstract"
 INTRO_MATCHSTR = "I.[\s]{1,3}(INTRODUCTION|Introduction|introduction)"
 REF_MATCHSTR = "Reference|REFERENCE|Bibliography"
 APD_MATCHSTR = "APPENDIX|Appendix"  # Not used for now
-# General
+# General title
 # FIXME: Misidentification exists
 SECTION_TITLE_MATCHSTR = ["[IVX1-9]{1,4}[\.\s][\sA-Za-z]{1,}|[1-9]{1,2}[\s\.\n][\sA-Za-z]{1,}",  # Level 1
                           "[A-M]{1}\.[\sA-Za-z]{1,}|[1-9]\.[1-9]\.[\sA-Za-z]{1,}"]  # Level 2
